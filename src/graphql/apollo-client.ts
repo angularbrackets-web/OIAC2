11c965642bb9cdb0e81e3ef1f885d3e6827f18fb
angularbrackets-web <angularbrackets.web@gmail.com>
2023-11-03 23:20:40 -0600

    refactor apollo client import

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,6 +1,5 @@
-import { InMemoryCache } from "@apollo/client"
-import { ApolloClient } from "@apollo/client/core/ApolloClient"
-
+import pkg from '@apollo/client'
+const { InMemoryCache, ApolloClient } = pkg
 
 const client = new ApolloClient({
     uri: 'https://api-us-west-2.hygraph.com/v2/clo0kf2c97pnm01t4g8sva2ni/master',
