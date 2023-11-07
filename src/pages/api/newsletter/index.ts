f666c4e8e2af2ae1252a018c99657381aab6b784
angularbrackets-web <angularbrackets.web@gmail.com>
2023-11-06 19:40:39 -0700

    updated mailchimp api key

--- src/pages/api/newsletter/index.ts
+++ src/pages/api/newsletter/index.ts
@@ -11,26 +11,26 @@ import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({request}) => {
     const subscriber = await request.json()
-    
+    console.log('Request : ', JSON.stringify(subscriber))
 
     try{
         const apiResponse = await fetch('https://us5.api.mailchimp.com/3.0/lists/813c461f9a',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json',
-                Authorization: 'auth 2422ee7f7e8c82250b1375294d27fab9-us5'
+                Authorization: 'auth 20964213599e7cf20af50c5d21291bdd-us5'
             },
             body: JSON.stringify(subscriber)
         })
-        if(!apiResponse.ok){
-            throw new Error('Error',{
-                cause: { apiResponse }
-            })
-        }
+        // if(!apiResponse.ok){
+        //     throw new Error('Error',{
+        //         cause: { apiResponse }
+        //     })
+        // }
 
         const jsonResponse = await apiResponse.json()
         
-        
+        console.log('Response : ', JSON.stringify(jsonResponse))
         if(jsonResponse.errors.length>0){
             return new Response(JSON.stringify(jsonResponse),{
                 status:500,
