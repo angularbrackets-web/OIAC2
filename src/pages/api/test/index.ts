c69211bc33fd6fcbf035cc6f76fdf91207bc3fe9
angularbrackets-web <angularbrackets.web@gmail.com>
2023-12-18 21:01:24 -0700

    added xata configuration

--- src/pages/api/test/index.ts
+++ src/pages/api/test/index.ts
@@ -1,12 +1,33 @@
 import type { APIRoute } from "astro"
+// import { getXataClient } from "../../../xata"
+
+// const client = getXataClient()
+
+import { XataClient } from '../../../xata'
+ 
+const client = new XataClient({
+  apiKey: import.meta.env.XATA_API_KEY,
+  branch: import.meta.env.XATA_BRANCH
+})
 
 export const GET: APIRoute = async () => {
-    await console.log('TEST GET')
-    return new Response('Hello World!',{
-        status:200,
-        headers:{
-                    'Content-Type':'application/json'
-                }
-            })
+    try{
+        const testEvents = await client.db.TestEvents.getAll()
+        return new Response(JSON.stringify(testEvents),{
+            status:200,
+            headers:{
+                        'Content-Type':'application/json'
+                    }
+                })
+    }
+    catch(error){
+        return new Response(JSON.stringify(error),{
+            status:500,
+            headers:{
+                        'Content-Type':'application/json'
+                    }
+                })
+    }
+    
 }
 
