711c924420b5cc3622480c6cc87811a756c200f4
ibrahimizh <ibrahim_na@outlook.com>
2024-11-24 19:30:43 -0700

    Added Our Staff page

---
+++ src/graphql/staff.ts
@@ -0,0 +1,45 @@
+import pkg from '@apollo/client'
+import {client} from './apollo-client'
+const {gql} = pkg
+
+export interface Staff {
+    id: string;
+    name: string;
+    profilePicture?: {
+      url: string;
+    } | null;
+    staffType: {
+      name: string;
+    };
+    description?: {
+      html: string;
+    } | null;
+  }
+
+  export async function getStaffMembers() {
+    const result = await client.query({
+        query: gql`
+        query StaffMembers {
+        staffs {
+          id
+          name
+          profilePicture {
+            url
+          }
+          staffType {
+            name
+          }
+          description {
+            html
+          }
+        }
+      }
+        `
+  })
+
+  let staffMembers : Array<Staff> = []
+
+  staffMembers = result.data.staffs
+  
+  return staffMembers
+}
